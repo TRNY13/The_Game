9c5d9736089bf5cac6700f8cf216bc9878834335
Titus Rainey <titusrainey@gmail.com>
2021-08-14 23:50:17 -0400

    created files and connected

---
+++ app.js
@@ -0,0 +1,17 @@
+class Robots {
+    constructor(Name,HP,Attack,Accuracy){
+    this.Name=Name,
+    this.HP=HP,
+    this.Attack=Attack,
+    this.Accuracy=Accuracy
+}
+}
+
+Punch (Robot)
+ let chancetoPunch = Math.random()
+ if(this.Accuracy >= chancetoPunch){
+     Robot.HP -= this.Attack;
+     //I want to add sound effects for whenever the punch lands
+     if(Robot.HP <= 0)} // 1 of the players lose
+     else{ //Robot misses the attack
+ }
