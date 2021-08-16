f50cb22e2e45281355e3b663b16cc710380d936c
Titus Rainey <titusrainey@gmail.com>
2021-08-15 21:39:54 -0400

    created files and connected

--- app.js
+++ app.js
@@ -3,15 +3,18 @@ class Robots {
     this.Name=Name,
     this.HP=HP,
     this.Attack=Attack,
-    this.Accuracy=Accuracy
-}
+    this.Accuracy=math.random()
 }
 
-Punch (Robot)
+
+Punch (Robot){
  let chancetoPunch = Math.random()
  if(this.Accuracy >= chancetoPunch){
      Robot.HP -= this.Attack;
      //I want to add sound effects for whenever the punch lands
-     if(Robot.HP <= 0)} // 1 of the players lose
-     else{ //Robot misses the attack
- }
+     if(Robot.HP <= 0) {alert("You Lost round 1")} // 1 of the players lose
+ }else{ //Robot misses the attack
+ alert("You miss")//This is just a placeholder for now, I want to have the arms flail or something
+}
+}
+}
